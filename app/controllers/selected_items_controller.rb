1a96b6b2002f0b587c668f11b0a404e433a7122c
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-02-04 16:26:03 -0600

    Updated Items#edit to include display of size and color options

--- app/controllers/selected_items_controller.rb
+++ app/controllers/selected_items_controller.rb
@@ -30,6 +30,9 @@ class SelectedItemsController < ApplicationController
     @selected_item.item_id = params[:item_id]
     @selected_item.cart_id = params[:cart_id]
 
+
+
+
     respond_to do |format|
       if @selected_item.save
         format.html { redirect_to :back, notice: 'Item was successfully added to your cart.' }
@@ -75,6 +78,6 @@ class SelectedItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def selected_item_params
-      params.require(:selected_item).permit(:item_id, :cart_id, :user_id)
+      params.require(:selected_item).permit(:item_id, :cart_id, :user_id, :size_option, :color_option)
     end
 end
