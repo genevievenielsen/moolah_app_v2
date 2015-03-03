d0d3825db5f9ace9162907bdff1d0e4a43d3a91c
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-03-03 10:49:10 -0600

    Updated color and size options

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :size_options
-
   resources :color_options
+
+  resources :size_options
 
   get('/home', {:controller => 'users', :action => 'home'})
 
