751b0704ce7c4973996df554036a71cea16d0b4d
genevievenielsen <genevieve.b.nielsen@gmail.com>
2015-03-19 14:39:55 -0500

    Fixed the navbar on the homepage

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+
     $('div.hidden').fadeIn(1500).removeClass('hidden');
+
+    //this is the navbar scroll down thing
+    $(window).scroll(function() {
+      if($(this).scrollTop() > 100) {
+        $('.navbar-default').addClass('navbar_color');
+      } else {
+        $('.navbar-default').removeClass('navbar_color');
+      }
+    });
+
 });
